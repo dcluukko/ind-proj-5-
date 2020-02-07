95d13f1f018b85a684c329489ca3f00f82415602
Dana <dcluukko@gmail.com>
2020-02-07 11:42:17 -0800

    created first test should have inputted earth years

--- __tests__/galacticage.test.js
+++ __tests__/galacticage.test.js
@@ -4,7 +4,7 @@ describe('Galactic Age', () => {
 
   test('should have inputted Earth years', () => {
     var galacticage = new Galacticage(1);
-    expect(galacticage.side1).toEqual(2);
+    expect(galacticage.side1).toEqual(1);
 
   });
 });
